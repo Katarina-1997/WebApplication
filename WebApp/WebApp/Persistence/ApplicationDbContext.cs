2dc282a3e209aca5c557edb0d3df6a87f392accf
KatarinaCvetkov <katarina.cvetkov55@gmail.com>
2023-06-25 10:49:20 +0200

    AplicationDbContext - Db set

---
+++ WebApp/WebApp/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace WebApp.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            //Reference name connection string from web.config file 
+            : base("DefaultConnection")
+        {
+        }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
