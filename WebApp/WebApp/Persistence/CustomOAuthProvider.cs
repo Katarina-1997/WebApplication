372bcaeca97aa15912a1f515696de920d30c5749
KatarinaCvetkov <katarina.cvetkov55@gmail.com>
2023-06-25 10:53:12 +0200

    Perstenece - Providesr Jwt token format

---
+++ WebApp/WebApp/Persistence/CustomOAuthProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+
+namespace WebApp.Providers
+{
+    public class CustomOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ WebApp/WebApp/Providers/CustomJwtFormat.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin.Security;
+using System;
+
+namespace WebApp.Providers
+{
+    public class CustomJwtFormat : ISecureDataFormat<AuthenticationTicket>
+    {
+        private readonly string _issuer = string.Empty;
+        public CustomJwtFormat(string issuer)
+        {
+            _issuer = issuer;
+        }
+        public string Protect(AuthenticationTicket data)
+        {
+            throw new NotImplementedException();
+        }
+        public AuthenticationTicket Unprotect(string protectedText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
